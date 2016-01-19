6019bc6933eee15db6610c25e1c8054183c7d211
mario <mrsitofull@gmail.com>
2016-01-19 22:05:11 +0100

    Semifinalizado

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,13 +5,15 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class App {
-    private static String[] nombresFicheros = { "lliga-1.xml", "lliga-2.xml", "lliga-1.xml"};
+    private static String[] nombresFicheros = {"/lliga1.xml", "/lliga2.xml", "/lliga1.xml"};
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         SAXParserFactory fabrica = SAXParserFactory.newInstance();
         fabrica.setNamespaceAware(true);
         SAXParser parser = fabrica.newSAXParser();
+        //parser.parse(new File(App.class.getResource("/lliga1.xml").getFile()), new Processar());
         for (String fichero : nombresFicheros)
             parser.parse(new File(App.class.getResource(fichero).getFile()), new Processar());
     }
